9774e6229be5525f6463f1a078353f7fc193900b
LeandroCantero <lcantero18@gmail.com>
2021-06-07 16:36:40 -0300

    auth

--- disney_challenge/src/main/java/com/example/disney_challenge/controllers/HelloController.java
+++ disney_challenge/src/main/java/com/example/disney_challenge/controllers/HelloController.java
@@ -1,13 +1,54 @@
 package com.example.disney_challenge.controllers;
 
+import com.example.disney_challenge.auth.AuthRequest;
+import com.example.disney_challenge.auth.AuthResponse;
+import com.example.disney_challenge.services.MyUserDetailsService;
+import com.example.disney_challenge.util.JwtUtil;
+import net.bytebuddy.asm.Advice;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
+
     @RequestMapping(path = "/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthToken(@RequestBody AuthRequest authRequest) throws Exception{
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+            );
+        } catch (BadCredentialsException e){
+            throw new Exception("Incorrect username or password",e );
+        }
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authRequest.getUsername());
+
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthResponse(jwt));
     }
 }
